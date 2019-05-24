995a7707fb756e4e8695efec9b4f06a1b034e49d
Jun Miyazaki <jun.miyazaki918@gmail.com>
2019-05-24 16:16:59 +0900

    Cocktail with photo

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
   get    "cocktails",          to: "cocktails#index"
-  get    "cocktails/new",      to: "cocktails#new"
+  get    "cocktails/new",      to: "cocktails#new", as: :new_cocktail
   get    "cocktails/:id",      to: "cocktails#show", as: :cocktail
   post   "cocktails",          to: "cocktails#create", as: :cocktails_create
   # NB: The `show` route needs to be *after* `new` route.
   get "cocktails/:cocktail_id/doses", to: "doses#index", as: :doses
   get "cocktails/:cocktail_id/doses/new", to: "doses#new", as: :doses_new
-  post "cocktails/:cocktail_id/doses",    to: "doses#create", as: :doses_create
-  delete "doses/:id", to: "doses#destroy", as: :doses_destroy
+  post "cocktails/:cocktail_id/doses",    to: "doses#create", as: :cocktail_doses
+  delete "doses/:id", to: "doses#destroy", as: :dose
 
   post "cocktails/:cocktail_id/reviews",    to: "reviews#create", as: :reviews_create
 end
